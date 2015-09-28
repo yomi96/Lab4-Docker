1ecdd8cf7c21e3a1a85fd55e6bc01eb6a1310a16
yomi96 <C13406652@mydit.ie>
2015-09-28 15:49:34 +0000

    first push

--- hello.py
+++ hello.py
@@ -1,2 +1,7 @@
-git@github.com:yomi96/YomisCode.git
-print"Hello"
+my_str = input("Enter a string: ")
+my_str = my_str.casefold()
+rev_str = reversed(my_str)
+if list(my_str) == list(rev_str):
+  print("Its a palindrom")
+  else:
+    print("Its not a palindrome")
